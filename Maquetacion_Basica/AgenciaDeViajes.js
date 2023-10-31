9aaf7cc2e1b13e3b8ef8d0a1064a9db3ae101d95
indiana <indimar.w1@gmail.com>
2023-10-31 17:04:11 +0100

    ultima subida corregida

--- Maquetacion_Basica/AgenciaDeViajes.js
+++ Maquetacion_Basica/AgenciaDeViajes.js
@@ -11,7 +11,7 @@ function guardarInformacion(){
         nombre: nombre,
         origen: origen,
         destino: destino,
-        numeroDePersonas: numeroDePersonas,
+        numeroPersonas: numeroDePersonas,
         fecha: fecha,
     };
     personas.push(nuevoContacto);
@@ -36,27 +36,29 @@ function filtrarDestino(){
     resultados.innerHTML= " ";
 
     solicitudesFiltradas.forEach(persona => {
+        const div = document.createElement("div");
         const nombre = document.createElement("p");
         nombre.textContent = persona.nombre;
-        resultados.appendChild(nombre);
+        div.appendChild(nombre);
 
         const origen = document.createElement("p");
         origen.textContent = persona.origen;
-        resultados.appendChild(origen);
+        div.appendChild(origen);
 
         const destino = document.createElement("p");
         destino.textContent = persona.destino;
-        resultados.appendChild(destino);
+        div.appendChild(destino);
 
-        const numerosPersonas = document.createElement("p");
-        numerosPersonas.textContent = persona.numerosPersonas;
-        resultados.appendChild(numerosPersonas);
+        const numeroPersonas = document.createElement("p");
+        numeroPersonas.textContent = persona.numeroPersonas;
+        div.appendChild(numeroPersonas);
 
         const fecha = document.createElement("p");
         fecha.textContent = persona.fecha;
-        resultados.appendChild(fecha);   
+        div.appendChild(fecha);   
+        resultados.appendChild(div)
     });
-
+console.log(resultados)
     console.log("solicitudes para Canarias, Mallorca o Galicia:");
     console.log(solicitudesFiltradas);
 }
